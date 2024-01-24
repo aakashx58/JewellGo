6d97c98275f0d9fc534e1acdb8f8e7f7b0ffdc01
Aakash Rajbanshi <106716824+aakashx58@users.noreply.github.com>
2024-01-24 14:12:52 +0545

    first

---
+++ lib/repository/jewellery_repository.dart
@@ -0,0 +1,86 @@
+import 'package:flutter_scroll_animation/models/jewellery.dart';
+
+class JewelleryRepository {
+  /// Earnings
+  static List<JewelleryModel> earnings = [
+    JewelleryModel(
+      name: 'Adya fine pankhi fish hook earrings',
+      description:
+          'An Amazing pair of Adya fine pankhi fish hook earrings. All of our jewelry is in 92.5 hallmarked silver.',
+      price: 19.99,
+      image: 'https://cdn.shyle.in/wp-content/uploads/2020/11/IMG_9963.jpg',
+    ),
+    JewelleryModel(
+      name: 'Jhumri Intricate Carved Classic Chandbali',
+      description:
+          'An amazing pair of Jhumri Intricate Carved Classic Chandbali. All of our jewelry is Pure silver. Stamped with 92.5',
+      price: 19.99,
+      image:
+          'https://cdn.shyle.in/wp-content/uploads/2022/03/photo_2022-03-16_11-16-35-900x1167.jpg',
+    ),
+    JewelleryModel(
+      name: 'Anvaya Three Layered Carved Danglers',
+      description:
+          'An amazing pair of Anvaya Three Layered Carved Danglers. All of our Jewellery is handcrafted in Pure Silver , Well stamped with 92.5',
+      price: 22.99,
+      image:
+          'https://cdn.shyle.in/wp-content/uploads/2018/10/Anvaya-3-Layered-Carved-Danglers-1-900x900.jpg.webp',
+    ),
+  ];
+
+  /// Ring
+  static List<JewelleryModel> ring = [
+    JewelleryModel(
+      name: 'Mizoya Intricate Ghughri Drop Carry Motif Ring',
+      description:
+          'Max Length : 4.7cm, Width : 2cm | Size Adjustable | Well Stamped with 92.5',
+      price: 15.99,
+      image:
+          'https://cdn.shyle.in/wp-content/uploads/2024/01/mizoya-intricate-ghughri-drop-carry-motif-ring-2.jpg',
+    ),
+    JewelleryModel(
+      name: 'Mizoya Handpainted Classic Ring',
+      description:
+          'Length :  3.9 cm | Size Adjustable | Pure silver | Well stamped with 92.5',
+      price: 15.99,
+      image:
+          'https://cdn.shyle.in/wp-content/uploads/2024/01/photo_2024-01-13_16-46-18.jpg.webp',
+    ),
+    JewelleryModel(
+      name: 'Adya Intricate Chakra Ring',
+      description:
+          'Length :  3 cm | Size Adjustable | Pure silver | Well stamped with 92.5',
+      price: 19.99,
+      image:
+          'https://cdn.shyle.in/wp-content/uploads/2023/10/anvaya-intricate-chakra-ring-1.jpg.webp',
+    ),
+  ];
+
+  /// Diamond
+  static List<JewelleryModel> diamond = [
+    JewelleryModel(
+      name: 'Classic Ring with Channel Diamonds',
+      description:
+          'Available in: White Gold | Yellow Gold | Rose Gold | Platinum.',
+      price: 299.99,
+      image:
+          'https://eadn-wc01-8996525.nxedge.io/pub/media/catalog/product/g/r/gr1071-23_c.jpg',
+    ),
+    JewelleryModel(
+      name: 'Solitaire Ring with Surprise Diamonds',
+      description:
+          'Available in: White Gold | Yellow Gold | Rose Gold | Platinum.',
+      price: 399.99,
+      image:
+          'https://eadn-wc01-8996525.nxedge.io/pub/media/catalog/product/m/e/me2211q-207_a_22255.jpg',
+    ),
+    JewelleryModel(
+      name: 'Modern Emerald Cut Diamond Ring',
+      description:
+          'Available in: White Gold | Yellow Gold | Rose Gold | Platinum.',
+      price: 499.99,
+      image:
+          'https://eadn-wc01-8996525.nxedge.io/pub/media/catalog/product/m/e/mecec2920q-347_c.jpg',
+    ),
+  ];
+}
